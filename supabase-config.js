6c2aef893bbdaa06291b377d71864470305e867d
AwayToMoon <noblesseraizl@gmail.com>
2025-05-05 05:23:18 +0200

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
-
-// Supabase Konfiguration (direkt eingetragen)
-const supabaseUrl = 'https://wgfdtcjdeiuuaridxfct.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnZmR0Y2pkZWl1dWFyaWR4ZmN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0MTEzOTcsImV4cCI6MjA2MTk4NzM5N30.5l7lFKATCMVIPjEqfjopZ90gSk9eF6zHooV7-ejhtpg';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase }; 
