16cb898acc6b02c574050dae80fd0a8989583bcc
AwayToMoon <noblesseraizl@gmail.com>
2025-09-18 05:08:30 +0200

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,13 @@
+// Firebase-Konfiguration - Beispiel
+// Kopiere diese Datei zu 'config.js' und fülle deine echten Werte ein
+const firebaseConfig = {
+  apiKey: "DEIN_API_KEY_HIER",
+  authDomain: "dein-projekt.firebaseapp.com",
+  projectId: "dein-projekt-id",
+  storageBucket: "dein-projekt.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
+};
+
+// Exportiere die Konfiguration
+window.firebaseConfig = firebaseConfig;
