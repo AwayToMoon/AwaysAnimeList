8671e2dead70cd8d29275a44afb51e6713e159f4
AwayToMoon <noblesseraizl@gmail.com>
2025-09-18 05:27:24 +0200

    Add files via upload

---
+++ config.production.js
@@ -0,0 +1,13 @@
+// Firebase-Konfiguration für Vercel
+// Verwendet Environment Variables
+const firebaseConfig = {
+    apiKey: "AIzaSyABddVmY4XpKA-PJ9dgexTRiufjRHBHVpc",
+    authDomain: "awaysanimelist.firebaseapp.com",
+    projectId: "awaysanimelist",
+    storageBucket: "awaysanimelist.firebasestorage.app",
+    messagingSenderId: "306455007306",
+    appId: "1:306455007306:web:f6fbe465c0b039127016b3"
+  };
+
+// Exportiere die Konfiguration
+window.firebaseConfig = firebaseConfig;
